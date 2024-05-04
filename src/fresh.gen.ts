acb7875e16b8c4963880485a9d935a6a37218ce4
tomo-local <tomohiro.togashi@gmail.com>
2024-05-04 18:55:15 +0900

    add pokemon list page

--- src/fresh.gen.ts
+++ src/fresh.gen.ts
@@ -5,23 +5,29 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
+import * as $api_pokemon_id_ from "./routes/api/pokemon/[id].tsx";
+import * as $api_pokemon_index from "./routes/api/pokemon/index.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
+import * as $pokemon_list from "./routes/pokemon/list.tsx";
 import * as $Counter from "./islands/Counter.tsx";
-import { type Manifest as _Manifest } from "$fresh/server.ts";
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
+    "./routes/api/pokemon/[id].tsx": $api_pokemon_id_,
+    "./routes/api/pokemon/index.ts": $api_pokemon_index,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
+    "./routes/pokemon/list.tsx": $pokemon_list,
   },
   islands: {
     "./islands/Counter.tsx": $Counter,
   },
   baseUrl: import.meta.url,
-} satisfies _Manifest;
+} satisfies Manifest;
 
 export default manifest;
---
+++ src/routes/pokemon/list.tsx
@@ -0,0 +1,75 @@
+import { PageProps } from "$fresh/server.ts";
+import { Handlers } from "$fresh/server.ts";
+
+import { PokemonListItem } from "@/types/pokemon.ts";
+
+export const handler: Handlers = {
+  async GET(_req, ctx) {
+    const page = ctx.url.searchParams.get("page") || 1;
+    const limit = ctx.url.searchParams.get("limit") || 20;
+    const offset = (Number(page) - 1) * Number(limit);
+
+    const url = new URL(`${ctx.url.origin}/api/pokemon`);
+    url.searchParams.set("limit", String(limit));
+    url.searchParams.set("offset", String(offset));
+
+    const response = await fetch(url.toString());
+
+    const data = await response.json();
+
+    if (!data) {
+      return ctx.render("Failed to fetch Pokemon list", { status: 500 });
+    }
+
+    const resp = await ctx.render(data);
+
+    return resp;
+  },
+};
+
+export default function PokemonList(props: PageProps) {
+  const page = Number(props.url.searchParams.get("page")) || 1;
+
+  return (
+    <div className="h-screen w-full bg-gray-100">
+      <div className="container py-8 mx-auto">
+        <h1 className="text-3xl font-semibold mb-8 text-center">
+          ポケモン図鑑
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {props.data.map((pokemon: PokemonListItem, index: number) => (
+            <div
+              key={index}
+              className="relative bg-white rounded-lg shadow-md overflow-hidden"
+            >
+              <div className="text-center">
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold mb-2">{pokemon.name}</h2>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="flex justify-center mt-4">
+            <a
+              href={`${props.url.pathname}?page=${Number(page) - 1}`}
+              style={{ visibility: page === 1 ? "hidden" : "visible" }}
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            >
+              前へ
+            </a>
+          </div>
+          <div className="flex justify-center mt-4">
+            <a
+              href={`${props.url.pathname}?page=${Number(page) + 1}`}
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            >
+              次へ
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
