ceba21fc8c5b6269446fc2d35f047c908d21bb31
tomo-local <tomohiro.togashi@gmail.com>
2024-05-05 11:20:38 +0900

    fix

--- src/fresh.gen.ts
+++ src/fresh.gen.ts
@@ -2,6 +2,9 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
+// deno-lint-ignore-file
+// deno-fmt-ignore
+
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
