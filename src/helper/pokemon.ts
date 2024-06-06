c57981933decaf3f01bcbe9131e3d91b9dae3926
tomo-local <tomohiro.togashi@gmail.com>
2024-06-06 22:57:46 +0900

    fix ci

--- src/helper/pokemon.ts
+++ src/helper/pokemon.ts
@@ -5,7 +5,7 @@ import {
   PokemonSpecies,
   PokemonStat,
   PokemonType,
-  PokemonDetails
+  PokemonDetails,
 } from "@/types/pokemon.ts";
 
 const BASE_URL = "https://pokeapi.co/api/v2";
@@ -40,7 +40,7 @@ export function makePokemonApiUrl({
 export function getPokemonModel(
   pokemon: Pokemon,
   species: PokemonSpecies,
-  lang?: string,
+  lang?: string
 ): PokemonListItemCustom {
   lang = lang || "ja";
 
@@ -75,7 +75,7 @@ export function getPokemonDetailModel(
     name: species.names.find((n) => n.language.name === lang)?.name || "",
     genera: species.genera.find((g) => g.language.name === lang)?.genus || "",
     flavor_text_entries: species.flavor_text_entries.filter(
-      (s) => s.language.name === lang,
+      (s) => s.language.name === lang
     ),
 
     images: pokemon.sprites,
@@ -94,7 +94,8 @@ export function getPokemonDetailModel(
         base_stat: stat.base_stat,
         effort: stat.effort,
         stat: {
-          name: s.find((n) => n.en_name === stat.stat.name)?.name || stat.stat.name,
+          name:
+            s.find((n) => n.en_name === stat.stat.name)?.name || stat.stat.name,
           url: stat.stat.url,
         },
       };
